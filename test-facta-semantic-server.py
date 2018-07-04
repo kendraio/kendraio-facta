c58249e8088778a645ecd03b73ea91c15eac7007
Neil Harris <neil@kendra.io>
2018-07-04 22:02:43 +0000

    added experimental semantic server: first hookup works

---
+++ test-facta-semantic-server.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+import sys
+sys.path.append('../kendraio-api')
+import kendraio_api_server, json, hashlib, sys, time, psycopg2, datetime
+import jsonschema
+
+# adapted from https://github.com/RDFLib/rdflib-sqlalchemy
+from rdflib import plugin, Graph, Literal, URIRef, BNode, RDF
+from rdflib.store import Store
+
+from rdflib_sqlalchemy import registerplugins
+
+# To use this, this code needs to be run as the user in question
+# Note also that pip install is in general user-local
+# Note that this DBURI does not specify host or username: instead, connection is 
+# made via UNIX domain sockets, which are self-authenticating
+DB_URI = Literal('postgresql+psycopg2:///kendraio_facta')
+
+# This identifies the set of database tables used for storage
+DB_ident = URIRef("rdflib_semantic_store")
+
+# open a store with this ident
+def setup_rdf_store(ident):
+    registerplugins()
+    store = plugin.get("SQLAlchemy", Store)(identifier=ident)
+    return store
+
+if __name__ == '__main__':
+    def assertion_handler(source_id, statements, context):
+        # Attempt to cnonicalize this object by round-tripping decoding and re-encoding
+        statements = json.loads(json.dumps(statements, sort_keys=True))
+
+        # check that we are being sent something that looks like a statement list
+        if type(statements) != list:
+            raise Exception("invalid data, wrong type")
+        if [1 for x in statements if (type(x) != dict) or ("@context" not in x)]:
+            raise Exception("invalid data, missing attribute")
+
+        # And now validate that in more detail for every statement in the list
+        for x in statements:
+            jsonschema.validate(x, context["json-ld-schema"])
+
+        assertion_time = datetime.datetime.now().isoformat()
+
+        # and write to database
+        conn = context["db-connection"]
+        cur = conn.cursor()
+        statements_text = json.dumps(statements, sort_keys=True)
+        cur.execute('INSERT INTO received_statements (time, subject, statement, sha256_hash) VALUES (%s,%s,%s,%s);',
+                    (assertion_time,
+                     source_id,
+                     statements_text,
+                     hashlib.sha256(statements_text).hexdigest()
+                    ))
+
+        conn.commit()
+        cur.close()
+
+        store = setup_rdf_store(DB_ident)
+        graph = Graph(store, identifier=URIRef("foobar"))
+        graph.open(DB_URI, create=True)
+
+        # and add this single triple to the store
+        graph.add((URIRef("this"), RDF.subject, URIRef("http://facta-test.kendra.io/")))
+        graph.close()
+
+        return {"received": statements}
+
+    # load credentials from stdin
+    credentials = json.loads(sys.stdin.read())
+
+    # passwordless login via UNIX sockets
+    conn = psycopg2.connect(dbname=credentials["POSTGRES_DATABASE"],
+                            user=credentials["POSTGRES_USERNAME"])
+    print "created database connection"
+
+    server = kendraio_api_server.api_server("localhost", int(sys.argv[1]))
+    print "created http server"
+
+    server.add_credentials(credentials)
+    server.add_handler('/assert', assertion_handler,
+                       context={"db-connection": conn,
+                                "json-ld-schema": json.loads(open("json-ld-schema.json").read())})
+    server.run()
+
